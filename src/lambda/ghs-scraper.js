2d70fdef1d35bf3d483caec7cde4faf9df8dc193
Baffour Adu Boampong <baffouraduboampong@gmail.com>
2020-03-20 23:11:39 +0000

    Added env variables

--- src/lambda/ghs-scraper.js
+++ src/lambda/ghs-scraper.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 exports.handler = async (event, context) => {
+  const { SCRAPER_AUTH_TOKEN } = process.env;
+
     return {
       statusCode: 200,
-      body: "Hi there, Akotoro"
+      body: `Hi there, ${SCRAPER_AUTH_TOKEN}`
     };
   };
