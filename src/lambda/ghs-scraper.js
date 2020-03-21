b2aa232bcf68c2da5f219e10a376fcd4c13258e9
Baffour Adu Boampong <baffouraduboampong@gmail.com>
2020-03-21 02:53:50 +0000

    Completed scraper and email notification functions

--- src/lambda/ghs-scraper.js
+++ src/lambda/ghs-scraper.js
@@ -1,10 +1,78 @@
-require('dotenv').config();
+import axios from "axios";
+import cheerio from "cheerio";
+
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Content-Type": "application/json"
+};
+const GHS_WEBSITE_URL = "https://ghanahealthservice.org/covid19/";
 
 exports.handler = async (event, context) => {
-  const { SCRAPER_AUTH_TOKEN } = process.env;
+  if (event.httpMethod !== "GET") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  try {
+    // fetch website html
+    const response = await axios.get(GHS_WEBSITE_URL);
+    let status_updates = [];
+
+    // load website html
+    const $ = cheerio.load(response.data);
+
+    // Get number of cases
+    const number_of_cases = $(".widget-box-content", ".widget-box")
+      .filter(index => index === 1)
+      .find("span")
+      .text();
+
+    // Get last case time details
+    const last_update_of_cases_formatted = $(".widget-box-title", ".widget-box")
+      .filter(index => index === 2)
+      .text()
+      .replace(/as at\s/g, "")
+      .replace(/G.*$/g, "");
+    const last_update_of_cases_date = last_update_of_cases_formatted
+      .replace(/\|.*$/g, "")
+      .trim();
+    const last_update_of_cases_time = last_update_of_cases_formatted
+      .replace(/^.*\|/g, "")
+      .trim();
+
+    // Get cases status updates
+    $(".widget-box-status-content").each(function(index, element) {
+      const content = $(this)
+        .find(".widget-box-status-text")
+        .text();
+      status_updates.push({
+        title: $(this)
+          .find(".user-status-title")
+          .text(),
+        body_formatted: content,
+        body_raw: content.replace(/\r?\n|\r/g, "").trim(),
+        image: $(this).is(".widget-box-picture")
+          ? $(this)
+              .find(".widget-box-picture")
+              .find("img")
+              .attr("src")
+          : null
+      });
+    });
 
     return {
       statusCode: 200,
-      body: `Hi there, ${SCRAPER_AUTH_TOKEN}`
+      headers,
+      body: JSON.stringify({
+        number_of_cases,
+        last_update_of_cases_formatted,
+        last_update_of_cases_date,
+        last_update_of_cases_time,
+        status_updates
+      })
     };
-  };
+  } catch (error) {
+    console.log({ error });
+    return { statusCode: 422, headers, body: String(error) };
+  }
+};
---
+++ src/lambda/ghs-send-email-notification.js
@@ -0,0 +1,40 @@
+import sendgridMail from "@sendgrid/mail";
+
+require("dotenv").config();
+
+const { SENDGRID_API_KEY } = process.env;
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Content-Type": "application/json"
+};
+
+const msg = {
+  to: "",
+  from: "baffouraduboampong@gmail.com",
+  subject: "COVID-19 Update from Ghana Health Service",
+  html: ""
+};
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const requestBody = JSON.parse(event.body);
+  sendgridMail.setApiKey(SENDGRID_API_KEY);
+  msg.to = requestBody.to;
+  msg.html = requestBody.body;
+
+  try {
+    await sendgridMail.sendMultiple(msg);
+    return {
+      statusCode: 200,
+      headers,
+      body: "Email queued successfully"
+    };
+  } catch (error) {
+    console.log({ error });
+    return { statusCode: 422, headers, body: String(error) };
+  }
+};
